c53015860571ace7dcdaef4f7a66eebbdef48395
vik <evelyntjhin@22:2b:58:30:85:7f.home>
2026-05-10 16:42:17 -0400

    Add spi peripheral

--- src/project.v
+++ src/project.v
@@ -29,29 +29,30 @@ module tt_um_uwasic_onboarding_Victoria_Tjhin (
   wire [7:0] en_reg_pwm_15_8;
   wire [7:0] pwm_duty_cycle;
 
-  spi_peripheral spi_inst(
-    .clk(clk),
-    .rst_n(rst_n),
-    .sclk(sclk),
-    .copi(copi),
-    .ncs(ncs),
-    .en_reg_out_7_0(en_reg_out_7_0),
-    .en_reg_out_15_8(en_reg_out_15_8),
-    .en_reg_pwm_7_0(en_reg_pwm_7_0),
-    .en_reg_pwm_15_8(en_reg_pwm_15_8),
-    .pwm_duty_cycle(pwm_duty_cycle)
+  spi_peripheral spi_peripheral_inst (
+    .clk              (clk),
+    .rst_n            (rst_n),
+    .sclk             (ui_in[0]),
+    .copi             (ui_in[1]),
+    .ncs              (ui_in[2]),
+    .en_reg_out_7_0   (en_reg_out_7_0),
+    .en_reg_out_15_8  (en_reg_out_15_8),
+    .en_reg_pwm_7_0   (en_reg_pwm_7_0),
+    .en_reg_pwm_15_8  (en_reg_pwm_15_8),
+    .pwm_duty_cycle   (pwm_duty_cycle)
   );
-  
+
   pwm_peripheral pwm_peripheral_inst (
-    .clk(clk),
-    .rst_n(rst_n),
-    .en_reg_out_7_0(en_reg_out_7_0),
-    .en_reg_out_15_8(en_reg_out_15_8),
-    .en_reg_pwm_7_0(en_reg_pwm_7_0),
-    .en_reg_pwm_15_8(en_reg_pwm_15_8),
-    .pwm_duty_cycle(pwm_duty_cycle),
-    .out({uio_out, uo_out})
+    .clk              (clk),
+    .rst_n            (rst_n),
+    .en_reg_out_7_0   (en_reg_out_7_0),
+    .en_reg_out_15_8  (en_reg_out_15_8),
+    .en_reg_pwm_7_0   (en_reg_pwm_7_0),
+    .en_reg_pwm_15_8  (en_reg_pwm_15_8),
+    .pwm_duty_cycle   (pwm_duty_cycle),
+    .out              ({uio_out, uo_out})
   );
+
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
 
